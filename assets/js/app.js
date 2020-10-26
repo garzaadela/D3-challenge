9f4fa4be0cacbd21ae98e22b2268f64a51018ba2
Adela Garza <a.garza045@gmail.com>
2020-10-26 16:09:22 -0500

    updated hw

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ let margin = {
 let width = svgWidth - margin.left - margin.right;
 let height = svgHeight - margin.top - margin.bottom;
 
-let svg = d3.select(".container")
+let svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
@@ -19,8 +19,32 @@ let svg = d3.select(".container")
 let containerGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+// initial params
+// Y axis is healthcare
+// X axis is poverty
+// imported the data
 d3.csv("assets/data/data.csv").then(function(stateData) {
-    stateData.poverty = +stateData.poverty;
-    stateData.healthcare = +stateData.healthcare;
-    console.log(stateData);
+    // parse data/cast as numbers
+    stateData.forEach(function(data) {
+        stateData.poverty = +stateData.poverty;
+        stateData.healthcare = +stateData.healthcare;
+
+    });
+    
+    // create scale functions
+    let xLinearScale = d3.scaleLinear()
+        .domain([20, d3.max(stateData, d => d.poverty)])
+        .range([0, width]);
+
+    let yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(stateData, d => d.healthcare)])
+        .range([height, 0]);
+
+    // create axis functions
+    let bottomAxis = d3.axisBottom(xLinearScale);
+    let leftAxis = d3.axisLeft(yLinearScale);
+
+    // append Axes to the chart
+    
+
 });
